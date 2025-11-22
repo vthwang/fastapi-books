bfcd9721ca0b9f1b712b7a275e65335ffac4c5d7
vthwang <vincent@fishboneapps.com>
2025-11-22 03:22:00 -0800

    feat: add validation
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- books.py
+++ books.py
@@ -7,41 +7,39 @@ BOOKS = [
         "id": 1,
         "title": "The Great Gatsby",
         "author": "F. Scott Fitzgerald",
-        "category": "Classics"
+        "category": "Classics",
     },
-    {
-        "id": 2,
-        "title": "1984",
-        "author": "George Orwell",
-        "category": "Dystopian"
-    },
+    {"id": 2, "title": "1984", "author": "George Orwell", "category": "Dystopian"},
     {
         "id": 3,
         "title": "Don't Look Up",
         "author": "Adam McKay",
-        "category": "Science Fiction"
+        "category": "Science Fiction",
     },
     {
         "id": 4,
         "title": "The Catcher in the Rye",
         "author": "J.D. Salinger",
-        "category": "Classics"
+        "category": "Classics",
     },
     {
         "id": 5,
         "title": "The Lord of the Rings",
         "author": "J.R.R. Tolkien",
-        "category": "Fantasy"
+        "category": "Fantasy",
     },
 ]
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+
 @app.get("/books")
 def read_all_books():
     return BOOKS
+
 
 @app.get("/books/{book_title}")
 def read_book(book_title: str):
@@ -49,6 +47,7 @@ def read_book(book_title: str):
         if book.get("title").casefold() == book_title.casefold():
             return book
     return {"message": "Book not found"}
+
 
 @app.get("/books/")
 def read_books_by_category(category: str):
@@ -58,18 +57,24 @@ def read_books_by_category(category: str):
             books_to_return.append(book)
     return books_to_return
 
+
 @app.get("/books/{book_author}/")
 def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get("author").casefold() == book_author.casefold() and book.get("category").casefold() == category.casefold():
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
             books_to_return.append(book)
     return books_to_return
+
 
 @app.post("/books")
 def create_book(new_book: dict = Body()):
     BOOKS.append(new_book)
     return new_book
+
 
 @app.put("/books/{book_id}")
 def update_book(book_id: int, updated_book: dict = Body()):
@@ -78,6 +83,7 @@ def update_book(book_id: int, updated_book: dict = Body()):
             BOOKS[i] = updated_book
             return BOOKS[i]
     return {"message": "Book not found"}
+
 
 @app.delete("/books/{book_id}")
 def delete_book(book_id: int):
--- books2.py
+++ books2.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
+
 
 class Book:
     id: int
@@ -16,15 +19,66 @@ class Book:
         self.description = description
         self.rating = rating
 
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+
 BOOKS = [
-    Book(1, "The Great Gatsby", "F. Scott Fitzgerald", "A classic novel about the American Dream", 4.5),
-    Book(2, "1984", "George Orwell", "A dystopian novel about a totalitarian society", 4.0),
-    Book(3, "Don't Look Up", "Adam McKay", "A science fiction novel about a comet that is heading towards Earth", 3.5),
-    Book(4, "The Catcher in the Rye", "J.D. Salinger", "A novel about a young man's journey of self-discovery", 4.0),
-    Book(5, "The Lord of the Rings", "J.R.R. Tolkien", "A fantasy novel about a journey to destroy a ring", 5.0),
+    Book(
+        1,
+        "The Great Gatsby",
+        "F. Scott Fitzgerald",
+        "A classic novel about the American Dream",
+        4.5,
+    ),
+    Book(
+        2,
+        "1984",
+        "George Orwell",
+        "A dystopian novel about a totalitarian society",
+        4.0,
+    ),
+    Book(
+        3,
+        "Don't Look Up",
+        "Adam McKay",
+        "A science fiction novel about a comet that is heading towards Earth",
+        3.5,
+    ),
+    Book(
+        4,
+        "The Catcher in the Rye",
+        "J.D. Salinger",
+        "A novel about a young man's journey of self-discovery",
+        4.0,
+    ),
+    Book(
+        5,
+        "The Lord of the Rings",
+        "J.R.R. Tolkien",
+        "A fantasy novel about a journey to destroy a ring",
+        5.0,
+    ),
 ]
+
 
 @app.get("/books")
 def read_all_books():
     return BOOKS
 
+
+@app.post("/books")
+def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+    return new_book
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
